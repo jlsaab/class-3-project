6925fa022c3465ba773b270e55afa3eabe7d1d57
Jessica Saab <93544068+jlsaab@users.noreply.github.com>
2024-04-11 00:57:01 -0400

    Set up base controls and background
    Added a search bar, zoom and rotate buttons, a toggle menu of background layer, and more content around the map

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # class-3-project
- first map
+
+# Map of Places I Lived Growing Up
+
+This map is for class 3 in Advanced GIS: Interactive Web Mapping and Spatial Data Visualization.
+
+I've dreamed of this map all my life! I always wanted to map each point and see a flyover effect to feel the scale anew and reminisce on the beautiful locations.
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,32 @@
-
 // my mapbox token
 mapboxgl.accessToken = 'pk.eyJ1IjoiamxzYWFiIiwiYSI6ImNsdWx2MWw0ajBuODgybG13NGJwdm1yYTYifQ.wYroyDZcY8OYGXc-qzEmdA';
 
 // constructing the map element
 const map = new mapboxgl.Map({
-    container: 'map-container', // container ID
+    container: 'map', // container ID
     center: [-73.99, 40.73], // starting position [lng, lat]
-    zoom: 10, // starting zoom
-    bearing: 29,
-    style: 'mapbox://styles/mapbox/navigation-day-v1'
+    zoom: 3, // starting zoom
+    bearing: 10,
+    style: 'mapbox://styles/mapbox/satellite-streets-v12'
 });
+
+// adding a search bar for addresses
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    }));
+
+// adding zoom and rotate buttons
+map.addControl(new mapboxgl.NavigationControl());
+
+// adding functionality to layer menu
+const layerList = document.getElementById('menu');
+const inputs = layerList.getElementsByTagName('input');
+
+for (const input of inputs) {
+    input.onclick = (layer) => {
+        const layerId = layer.target.id;
+        map.setStyle('mapbox://styles/mapbox/' + layerId);
+    }
+};
